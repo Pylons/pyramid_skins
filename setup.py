937ec024008ff68462659e096c08eb97ace96234
Malthe Borch <mborch@gmail.com>
2009-11-26 12:42:38 +0000

    Preparing release.

--- setup.py
+++ setup.py
@@ -52,7 +52,7 @@ tests_require = install_requires + [
     ] + tests_require
 
 setup(name='repoze.bfg.skins',
-      version = '0.17',
+      version = '0.18',
       description='Skin support for BFG.',
       long_description=long_description,
       keywords = "zope3 repoze bfg",
