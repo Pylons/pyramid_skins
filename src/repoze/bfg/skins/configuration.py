ab2b99a5a76cdc7f563b53e034873ff3218553cb
Wichert Akkerman <wichert@wiggy.net>
2010-03-11 10:45:50 +0000

    Be consistent with the rest of BFG and assume the path is relative to the caller

--- src/repoze/bfg/skins/configuration.py
+++ src/repoze/bfg/skins/configuration.py
@@ -1,8 +1,10 @@
 from repoze.bfg.skins.zcml import skins
+from repoze.bfg.path import caller_path
 from zope.configuration.config import ConfigurationMachine
 
 
 def register_path(path, discovery=False, indexes=[]):
+    path = caller_path(path)
     context = ConfigurationMachine()
     stmt = skins(context, path, discovery)
     for index in indexes:
