736984079a771506410dbad795ce38302715debf
Andreas Zeidler <az@zitc.de>
2011-04-01 11:33:50 +0000

    extend the helper from r8406 to allow passing in a request type

--- src/repoze/bfg/skins/configuration.py
+++ src/repoze/bfg/skins/configuration.py
@@ -3,10 +3,10 @@ from repoze.bfg.path import caller_path
 from zope.configuration.config import ConfigurationMachine
 
 
-def register_path(path, discovery=False, indexes=[]):
+def register_path(path, discovery=False, indexes=[], request_type=None):
     path = caller_path(path)
     context = ConfigurationMachine()
-    stmt = skins(context, path, discovery)
+    stmt = skins(context, path, discovery, request_type)
     for index in indexes:
         stmt.view(context, index)
     stmt.configure()
