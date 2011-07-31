332011425c83e48ba93e872bd9f8afa0e5ba02ff
Malthe Borch <mborch@gmail.com>
2011-07-31 09:35:21 +0200

    Updating maintainer metadata.

--- src/pyramid_skins/docs/conf.py
+++ src/pyramid_skins/docs/conf.py
@@ -38,7 +38,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Pyramid skins'
-copyright = u'2009-2011 Malthe Borch and the Repoze Community'
+copyright = u'2009-2011 Malthe Borch and the Pylons Community'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
